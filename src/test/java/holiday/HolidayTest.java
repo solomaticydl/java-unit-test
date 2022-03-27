b8b475e1b7f0ef5c7e919663d12d7f1f344afa2a
Tony Yang <tony.yang@langlive.com>
2022-03-27 11:46:46 +0800

    holiday: refactor test

--- src/test/java/holiday/HolidayTest.java
+++ src/test/java/holiday/HolidayTest.java
@@ -1,5 +1,6 @@
 package holiday;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -8,11 +9,25 @@ import static org.junit.Assert.assertEquals;
 
 public class HolidayTest {
 
+    private HolidayForTest holiday;
+
+    @Before
+    public void setUp() throws Exception {
+        holiday = new HolidayForTest();
+    }
+
     @Test
     public void today_is_xmas() {
-        HolidayForTest holiday = new HolidayForTest();
-        holiday.today = LocalDate.of(2000, 12, 25);
-        assertEquals("Merry Christmas", holiday.sayHello());
+        givenToday(12, 25);
+        sayHelloShouldBe("Merry Christmas");
+    }
+
+    private void sayHelloShouldBe(String expected) {
+        assertEquals(expected, holiday.sayHello());
+    }
+
+    private void givenToday(int month, int dayOfMonth) {
+        holiday.today = LocalDate.of(2000, month, dayOfMonth);
     }
 
     // Extract and Override
