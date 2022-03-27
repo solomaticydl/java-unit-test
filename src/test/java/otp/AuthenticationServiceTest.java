920096aa6eba46f3f62fdf22035d1d4d78fbe616
Tony Yang <tony.yang@langlive.com>
2022-03-27 16:21:23 +0800

    refactor unint test

--- src/test/java/otp/AuthenticationServiceTest.java
+++ src/test/java/otp/AuthenticationServiceTest.java
@@ -2,6 +2,7 @@ package otp;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -9,15 +10,41 @@ import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
-    @Test
-    public void is_valid_test() {
-        IProfile profile = mock(ProfileDao.class);
-        when(profile.getPassword("joey")).thenReturn("91");
-        IToken token = mock(RsaTokenDao.class);
-        when(token.getRandom(anyString())).thenReturn("000000");
+    private final IProfile profile = mock(IProfile.class);
+    private final IToken token = mock(IToken.class);
+    private final AuthenticationService target = new AuthenticationService(profile, token);
 
-        AuthenticationService target = new AuthenticationService(profile, token);
-        boolean actual = target.isValid("joey", "91000000");
+    @Test
+    public void is_valid() {
+        givenPassword("joey", "91");
+        givenToken(anyString(), "000000");
+
+        shouldBeValid("joey", "91000000");
+    }
+
+    @Test
+    public void is_invalid() {
+        givenPassword("joey", "91");
+        givenToken(anyString(), "000000");
+
+        shouldBeInvalid("joey", "wrong password");
+    }
+
+    private void shouldBeValid(String account, String password) {
+        boolean actual = target.isValid(account, password);
         assertTrue(actual);
+    }
+
+    private void shouldBeInvalid(String account, String password) {
+        boolean actual = target.isValid(account, password);
+        assertFalse(actual);
+    }
+
+    private void givenToken(String account, String password) {
+        when(token.getRandom(account)).thenReturn(password);
+    }
+
+    private void givenPassword(String account, String password) {
+        when(profile.getPassword(account)).thenReturn(password);
     }
 }
