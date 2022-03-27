7b2778a3e63b1117573aae52493ea31ef57a5f89
Tony Yang <tony.yang@langlive.com>
2022-03-27 16:03:59 +0800

    stub with mockito

--- src/test/java/otp/AuthenticationServiceTest.java
+++ src/test/java/otp/AuthenticationServiceTest.java
@@ -3,34 +3,21 @@ package otp;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
     @Test
     public void is_valid_test() {
-        AuthenticationService target = new AuthenticationService(new FakeProfile(), new FakeToken());
+        IProfile profile = mock(ProfileDao.class);
+        when(profile.getPassword("joey")).thenReturn("91");
+        IToken token = mock(RsaTokenDao.class);
+        when(token.getRandom(anyString())).thenReturn("000000");
 
+        AuthenticationService target = new AuthenticationService(profile, token);
         boolean actual = target.isValid("joey", "91000000");
-
         assertTrue(actual);
-    }
-}
-
-class FakeProfile implements IProfile {
-
-    @Override
-    public String getPassword(String account) {
-        if ("joey".equals(account)) {
-            return "91";
-        }
-        return "";
-    }
-}
-
-class FakeToken implements IToken {
-
-    @Override
-    public String getRandom(String account) {
-        return "000000";
     }
 }
